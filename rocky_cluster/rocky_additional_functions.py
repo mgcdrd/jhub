38e249a096d986fdd5aca3573c7077cb23435da3
Will Provenza <2-provenzaw@users.noreply.gitlab.lab.provenzawt.dev>
2022-10-25 16:52:09 -0400

    trying another debug option 13

--- rocky_cluster/rocky_additional_functions.py
+++ rocky_cluster/rocky_additional_functions.py
@@ -1,10 +1,15 @@
 from tornado import gen
+from pprint import pprint #debug
 import profiles
 
 @gen.coroutine
 def get_profiles(spawner):
 
-  print(spawner.user['oauth_user'].groups)
+  #print(spawner.user['oauth_user'].groups)
+  pprint(vars(spawner))
+
+  auth_state = yield spawner.user.get_auth_state()
+  print(auth_state['oauth_user']['groups'])
 
   profile_list = []
   profile_list.append(profiles.small_instance)
