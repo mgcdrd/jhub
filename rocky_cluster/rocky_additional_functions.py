8341aeaeb1cee9051773e78863262db84eaa182d
Will Provenza <2-provenzaw@users.noreply.gitlab.lab.provenzawt.dev>
2022-10-25 11:05:00 -0400

    trying to print out spawner info

---
+++ rocky_cluster/rocky_additional_functions.py
@@ -0,0 +1,13 @@
+@gen.coroutine
+def get_profiles(spawner):
+  
+  print(vars(spawner))
+  
+  profiles = []
+
+  profiles.append(small_instance)
+  profiles.append(medium_instance)
+
+  return profiles
+
+c.KubeSpawner.profile_list = get_profiles
