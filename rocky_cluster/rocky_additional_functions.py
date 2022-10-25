8c3ba7e87d6e079a88a276ea28a5316f55554437
Will Provenza <2-provenzaw@users.noreply.gitlab.lab.provenzawt.dev>
2022-10-25 16:57:33 -0400

    testing profile based on group mbrship

--- rocky_cluster/rocky_additional_functions.py
+++ rocky_cluster/rocky_additional_functions.py
@@ -1,19 +1,18 @@
 from tornado import gen
-from pprint import pprint #debug
 import profiles
 
 @gen.coroutine
 def get_profiles(spawner):
 
-  #print(spawner.user['oauth_user'].groups)
-  pprint(vars(spawner))
 
   auth_state = yield spawner.user.get_auth_state()
-  print(auth_state['oauth_user']['groups'])
+  #print(auth_state['oauth_user']['groups']) #debug
 
   profile_list = []
   profile_list.append(profiles.small_instance)
-  profile_list.append(profiles.medium_instance)
+  
+  if 'grp-jhub-admin' in auth_state['oauth_user']['groups']:
+    profile_list.append(profiles.medium_instance)
 
   return profile_list
 
