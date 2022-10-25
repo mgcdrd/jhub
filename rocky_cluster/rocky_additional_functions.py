43aa28467110929a7c176c24513045a09154b64b
Will Provenza <2-provenzaw@users.noreply.gitlab.lab.provenzawt.dev>
2022-10-25 16:13:12 -0400

    trying another debug option 6

--- rocky_cluster/rocky_additional_functions.py
+++ rocky_cluster/rocky_additional_functions.py
@@ -4,21 +4,16 @@ import profiles
 @gen.coroutine
 def get_profiles(spawner):
 
-  #print(vars(spawner))
+  print(spawner.user.auth_state)
 
   profile_list = []
-
   profile_list.append(profiles.small_instance)
   profile_list.append(profiles.medium_instance)
 
   return profile_list
 
-#debug
-def print_auth_hook(authenticator, handler, authentication):
-  print(vars(authenticator))
-  return authentication
 
 #debug
 def userdata_hook(spawner, auth_state):
-  print(auth_state)
+  #print(auth_state)
   spawner.userdata = auth_state["userdata"]
