aefef4d6700e3a9dfbbe7e94d6ffce4a143ac4e5
Will Provenza <2-provenzaw@users.noreply.gitlab.lab.provenzawt.dev>
2022-10-25 11:25:33 -0400

    trying to print out spawner info 5

--- rocky_cluster/rocky_additional_functions.py
+++ rocky_cluster/rocky_additional_functions.py
@@ -1,15 +1,15 @@
 from tornado import gen
 
-    @gen.coroutine
-    def get_profiles(spawner):
-    
-    print(vars(spawner))
-    
-    profiles = []
+@gen.coroutine
+def get_profiles(spawner):
 
-    profiles.append(small_instance)
-    profiles.append(medium_instance)
+  print(vars(spawner))
 
-    return profiles
+  profiles = []
 
-    c.KubeSpawner.profile_list = get_profiles
+  profiles.append(small_instance)
+  profiles.append(medium_instance)
+
+  return profiles
+
+c.KubeSpawner.profile_list = get_profiles
