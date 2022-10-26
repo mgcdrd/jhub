9b8771e82cd7075b4e9b53052cc4f4af2d6f283c
Will Provenza <2-provenzaw@users.noreply.gitlab.lab.provenzawt.dev>
2022-10-26 11:38:32 -0400

    working on env setup

--- rocky_cluster/rocky_additional_functions.py
+++ rocky_cluster/rocky_additional_functions.py
@@ -4,9 +4,7 @@ import profiles
 @gen.coroutine
 def get_profiles(spawner):
 
-
   auth_state = yield spawner.user.get_auth_state()
-  #print(auth_state['oauth_user']['groups']) #debug
 
   profile_list = []
   profile_list.append(profiles.small_instance)
@@ -17,7 +15,25 @@ def get_profiles(spawner):
   return profile_list
 
 
+@gen.coroutine
+def set_env(spawner):
+
+  auth_state = yield spawner.user.get_auth_state()
+
+  spawner.uid = #the uid from the get__auth_state
+  supplemental_gids
+  gid
+
+
+
+  volumes #just like volumes on a pod
+  volume_mounts #just like volumeMounts on a pod
+  working_dir
+
+
+
+
 #debug
-def userdata_hook(spawner, auth_state):
+def userdata_hook(spawner):
   #print(auth_state)
   spawner.userdata = auth_state["userdata"]
