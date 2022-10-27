bfa91750e2043306d4f18d83997fed5eb01e4b5a
Will Provenza <2-provenzaw@users.noreply.gitlab.lab.provenzawt.dev>
2022-10-27 14:24:03 -0400

    adding startup.sh command to profiles

--- rocky_cluster/rocky_profiles.py
+++ rocky_cluster/rocky_profiles.py
@@ -61,13 +61,15 @@ medium_instance = {
         "latest": {
           "display_name": "singleuser:latest",
           "kubespawner_override": {
-            "image": "registry.lab.provenzawt.dev/forme/jhub-tensorflow:latest"
+            "image": "registry.lab.provenzawt.dev/forme/jhub-tensorflow:latest",
+            "cmd": "/startup.sh"
           }
         },
         "3.0": {
           "display_name": "singleuser:3.0",
           "kubespawner_override": {
-            "image": "registry.lab.provenzawt.dev/forme/jhub-tensorflow:latest"
+            "image": "registry.lab.provenzawt.dev/forme/jhub-tensorflow:latest",
+            "cmd": "/startup.sh"
           }
         }
       }
