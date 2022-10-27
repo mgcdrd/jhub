49c38e014f99245451e9101e079c78ed0a262391
Will Provenza <2-provenzaw@users.noreply.gitlab.lab.provenzawt.dev>
2022-10-27 18:17:25 -0400

    env casting str to int

--- rocky_cluster/rocky_additional_functions.py
+++ rocky_cluster/rocky_additional_functions.py
@@ -20,9 +20,9 @@ def set_env(spawner):
   
   auth_state = yield spawner.user.get_auth_state()
 
-  spawner.uid = auth_state['oauth_user']['uid']
-  spawner.gid = auth_state['oauth_user']['gid']
-  spawner.fs_gid = auth_state['oauth_user']['fs_gid']
+  spawner.uid = int(auth_state['oauth_user']['uid'])
+  spawner.gid = int(auth_state['oauth_user']['gid'])
+  spawner.fs_gid = int(auth_state['oauth_user']['fs_gid'])
   spawner.environment['NB_USER'] = auth_state['oauth_user']['preferred_username']
   spawner.environment['NB_UID'] = auth_state['oauth_user']['uid']
   spawner.environment['NB_GID'] = auth_state['oauth_user']['gid']
