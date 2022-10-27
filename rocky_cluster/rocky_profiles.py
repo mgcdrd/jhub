1de077641ddb43d520bb0f29fdf89e0c93dd3537
Will Provenza <2-provenzaw@users.noreply.gitlab.lab.provenzawt.dev>
2022-10-27 15:21:04 -0400

    working on startup scripts 2

--- rocky_cluster/rocky_profiles.py
+++ rocky_cluster/rocky_profiles.py
@@ -62,14 +62,14 @@ medium_instance = {
           "display_name": "singleuser:latest",
           "kubespawner_override": {
             "image": "registry.lab.provenzawt.dev/forme/jhub-tensorflow:latest",
-            "cmd": "custom_startup"
+            "cmd": "/usr/local/bin/custom_startup"
           }
         },
         "3.0": {
           "display_name": "singleuser:3.0",
           "kubespawner_override": {
             "image": "registry.lab.provenzawt.dev/forme/jhub-tensorflow:latest",
-            "cmd": "custom_startup"
+            "cmd": "/usr/local/bin/custom_startup"
           }
         }
       }
